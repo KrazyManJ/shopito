44b833e1ee1d4d6eb0d818ab30610cbdd5dfd03b
KrazyManJ <KrazyManJ@gmail.com>
2026-01-10 23:24:53 +0100

    Removed unwanted log

--- app/src/main/java/dev/krazymanj/shopito/ui/screens/shoppingListsSummary/ShoppingListsSummaryScreen.kt
+++ app/src/main/java/dev/krazymanj/shopito/ui/screens/shoppingListsSummary/ShoppingListsSummaryScreen.kt
@@ -161,7 +161,6 @@ fun ShoppingListsSummaryScreenContent(
                         actions.setCurrentViewingShoppingItem(it)
                     },
                     onRemove = {
-                        Log.i("ShoppingListsSummaryScreenContent","Remove ${it.item.itemName}")
                         actions.deleteShoppingItem(it.item)
                     }
                 )
